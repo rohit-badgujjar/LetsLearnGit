d79339cc1738f3fa73513610f1b7d9874aa74f14
Rohit Badgujjar <rohitbadgujar937@gmail.com>
2024-11-04 00:38:24 +0530

    added message in sumofnums.cpp

---
+++ sumofnums.cpp
@@ -0,0 +1,11 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+
+int main(){
+    cout<<"Enter num a and b : ";
+    int a,b; 
+    cin>>a>>b;
+    cout<<"Sum of : "<<a<<" and "<<b<<" is :"<<a+b<<endl;
+    return 0;
+}
